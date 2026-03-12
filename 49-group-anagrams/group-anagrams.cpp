0d52000b1c1b988841d4fb64c404ace5ebef2a6a
Anantharam Pranay Kumar <anantharampranay6@gmail.com>
2026-03-12 19:59:23 +0530

    Time: 14 ms (74.07%) | Memory: 25 MB (66.36%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for(string s : strs){
-            string key = s;
-            sort(key.begin(), key.end());
-            mp[key].push_back(s);
+        unordered_map<string, vector<string>> mp; // creates a empty map with key and result fields
+        for(string s : strs){ // loop through given i/o strs
+            string key = s;//assign 1st string in strs as key
+            sort(key.begin(), key.end());//sort the key s i.e 1st word 
+            mp[key].push_back(s);//push that to the map[key] postion / field
         }
-        vector<vector<string>> result;
-        for(auto &it : mp){
-            result.push_back(it.second);
+        vector<vector<string>> result;//empty container for storing sorted strings 
+        for(auto &it : mp){ // loop thorugh the mp
+            result.push_back(it.second); // stores only the values i.e the sorted values into the result
         }
         return result;
     }
