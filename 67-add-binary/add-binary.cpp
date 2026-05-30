68e68b15dee82e05489ab8e321ced51d12f9f6df
Anantharam Pranay Kumar <anantharampranay6@gmail.com>
2026-05-30 23:15:51 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (58.81%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+    string res;
+    int cry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+    while (i >= 0 || j >= 0 || cry) {
+      if (i >= 0)
+        cry += a[i--] - '0';
+      if (j >= 0)
+        cry += b[j--] - '0';
+      res += cry % 2 + '0';
+      cry /= 2;
+    }
+    reverse(begin(res), end(res));
+    return res;
+    }
+};
